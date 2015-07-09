191b0be434b29e6a7047b08a6ebb25afd4817282
hlkcrcck <acarcicek@itu.edu.tr>
2015-07-09 11:26:26 +0300

    link problem

--- tr/assets/js/main.js
+++ tr/assets/js/main.js
@@ -3,4 +3,4 @@ var proto = window.location.protocol;
 var host = window.location.hostname;
 var path = window.location.pathname;
 path = path.replace("/tr/", "/en/");
-a.href = proto+"\\"+host+path;
+a.href = proto+"//"+host+path;
