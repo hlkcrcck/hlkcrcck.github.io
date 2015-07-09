c271666e92f036bd9d48238c211c273816a4891a
hlkcrcck <acarcicek@itu.edu.tr>
2015-07-09 11:27:38 +0300

    link problemi

--- en/assets/js/main.js
+++ en/assets/js/main.js
@@ -1,5 +1,6 @@
 var a = document.getElementById('turkce'); //or grab it by tagname etc
+var proto = window.location.protocol;
 var host = window.location.hostname;
 var path = window.location.pathname;
 path = path.replace("/en/", "/tr/");
-a.href = host+path;
+a.href = proto+"//"+host+path;
