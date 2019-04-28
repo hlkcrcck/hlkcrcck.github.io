7325940ebdd39475d83f8aeb4799f272849b09a9
hlkcrcck <acarcicek@itu.edu.tr>
2019-04-28 06:07:28 +0300

    tr removed, home update

--- tr/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-var a = document.getElementById('ingilizce'); //or grab it by tagname etc
-var proto = window.location.protocol;
-var host = window.location.hostname;
-var path = window.location.pathname;
-path = path.replace("/tr/", "/en/");
-a.href = proto+"//"+host+path;
